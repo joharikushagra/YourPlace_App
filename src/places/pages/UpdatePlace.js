be0c228498ab7f9ede3ebcf589e5ab0c73871e81
Kushagra Johari <skjkushagra1309@gmail.com>
2020-06-18 17:57:42 +0530

    last changes

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -39,7 +39,7 @@ function UpdatePlace() {
     const fetchPlace = async () => {
       try {
         const responseData = await sendRequest(
-          `http://localhost:5000/api/places/${placeId}`
+          `${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`
         );
 
         setLoadedPlace(responseData.place);
@@ -67,7 +67,7 @@ function UpdatePlace() {
     event.preventDefault();
     try {
       await sendRequest(
-        `http://localhost:5000/api/places/${placeId}`,
+        `${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`,
         "PATCH",
         JSON.stringify({
           title: formState.inputs.title.value,
